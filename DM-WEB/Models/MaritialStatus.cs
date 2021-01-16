f17b70e523c105b25cedd7aa507759f34b5af4c0
Danilo O Pinheiro <danilo.o.pinheiro@avanade.com>
2021-01-16 02:29:58 -0300

    novas implementações

---
+++ DM-WEB/Models/MaritialStatus.cs
@@ -0,0 +1,11 @@
+namespace DM_WEB.Models
+{
+    public enum MaritialStatus
+    {
+        NotMarried,
+        Married,
+        Divorced,
+        Widower,
+        Separate
+    }
+}
---
+++ DM-WEB/Models/Sex.cs
@@ -0,0 +1,8 @@
+namespace DM_WEB.Models
+{
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+}
